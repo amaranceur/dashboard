41f7cc207d3e569c9a9c35f208d33c990d8d21a9
amaranceur <amaranceur2@gmail.com>
2025-06-21 02:48:47 +0100

    update

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataContacts } from "../../data/mockData";
-import Header from "../../components/Heade.jsx";
+import Header from "../../components/Header.jsx";
 import { useTheme } from "@mui/material";
 
 const Contacts = () => {
